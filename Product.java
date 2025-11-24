0d54d935ea1b5cf6166b1e08d842869132d94500
Erikooh <erickwanyama1564@gmail.com>
2025-11-24 23:37:57 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,36 +1,36 @@
 // Classes & Objects - Question 2: Product with Discount
-// Author: Erick
+// Author: S08-8511-2024
 // Date: Nov 24, 2025
 
 import java.util.Scanner;
 
 public class Product {
-    // Private attributes
+   
     private String name;
     private double price;
 
-    // Constructor to set both attributes
+ 
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Method to apply discount
+   
     public void applyDiscount(double percentage) {
-        // Reduce price by given percentage
+       
         price = price - (price * (percentage / 100));
     }
 
-    // Method to return updated price
+  
     public double getPrice() {
         return price;
     }
 
-    // Main method to test Product class
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Create a Product object
+       
         Product product = new Product("Laptop", 1000.00);
 
         // Ask user for discount percentage
